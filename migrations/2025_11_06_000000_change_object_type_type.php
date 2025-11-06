e653b3de4cac464938d99a72c14235122078d499
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2025-11-06 16:52:41 +0100

    chore: basic sanity checks for odd environments

--- migrations/2025_11_06_000000_change_object_type_type.php
+++ migrations/2025_11_06_000000_change_object_type_type.php
@@ -5,9 +5,11 @@ use Illuminate\Database\Schema\Builder;
 
 return [
     'up' => function (Builder $schema) {
-        $schema->table('seo_meta', function (Blueprint $table) {
-            $table->string('object_type', 255)->change();
-        });
+        if ($schema->hasTable('seo_meta')) {
+            $schema->table('seo_meta', function (Blueprint $table) {
+                $table->string('object_type', 255)->change();
+            });
+        }
     },
     'down' => function (Builder $schema) {
         if ($schema->hasTable('seo_meta')) {
